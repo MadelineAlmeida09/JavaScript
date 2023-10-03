d5a5dee965f5ed3fb7419d60e8deaf632d3c4b92
Madeline <139805381+MadelineAlmeida09@users.noreply.github.com>
2023-10-03 15:02:56 -0300

    New commit

--- aula12-ex/modelo/script.js
+++ aula12-ex/modelo/script.js
@@ -1,1 +1,6 @@
-var msg = document.getElementById('msg')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem') 
+    var data = new Date()
+    var hora = data.getHours()
+}
